92056ef0c0bd8312a47e19fa03827544bc78d3d7
Conor Grogan <crgrogan@hotmail.com>
2021-06-21 17:04:59 +0100

    Add file loader to handle pdf extension

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,5 @@
 import "./style.css";
+import "../src/assets/DeveloperCV.pdf";
 
 document.addEventListener(
   "DOMContentLoaded",
--- client/webpack.common.js
+++ client/webpack.common.js
@@ -20,6 +20,18 @@ module.exports = {
         test: /\.(svg|png|jpg|gif|ico)$/,
         type: "asset/resource",
       },
+      {
+        test: /\.pdf$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "assets",
+            },
+          },
+        ],
+      },
     ],
   },
 };
